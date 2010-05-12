1b5b98330110ddfa88c9bf872886d7abac09f594
Caglar Gulcehre <caglar@ulakbim.gov.tr>
2010-05-12 06:43:51 +0300

    Initial Commit

---
+++ edu/cglr/lngamesml/Main.java
@@ -0,0 +1,15 @@
+package edu.src.lngamesml;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: caglar
+ * Date: May 12, 2010
+ * Time: 6:41:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main (String [] args){
+        
+    }
+}
---
+++ edu/cglr/lngamesml/Test.java
@@ -0,0 +1,11 @@
+package edu.src.lngamesml;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: caglar
+ * Date: May 12, 2010
+ * Time: 6:36:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
